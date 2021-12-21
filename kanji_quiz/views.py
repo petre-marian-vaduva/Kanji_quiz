36152fe48c1f489073e3ce054f48fedd17cf0c7d
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-12-21 07:04:49 +0000

    add kanji dictionary

--- kanji_quiz/views.py
+++ kanji_quiz/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+kanji_dict = {
+    '人': 'pessoa',
+    '十': 'dez',
+    '二': 'dois',
+    '九': 'nove',
+    '入': 'entrar ou inserir',
+    '八': 'oito',
+    '大': 'grande',
+    '上': 'acima',
+    '子': 'criança'
+}
+
